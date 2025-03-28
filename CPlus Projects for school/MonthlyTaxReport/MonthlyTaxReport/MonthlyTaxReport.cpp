32b7f9d2998af6bbd3c253aac03e4786cc80f927
ChoccyMilch <57919138+ChoccyMilch@users.noreply.github.com>
2025-03-28 12:36:37 -0400

    Update MonthlyTaxReport.cpp

--- CPlus Projects for school/MonthlyTaxReport/MonthlyTaxReport/MonthlyTaxReport.cpp
+++ CPlus Projects for school/MonthlyTaxReport/MonthlyTaxReport/MonthlyTaxReport.cpp
@@ -1,7 +1,3 @@
-/*A retail company must file a monthly sales tax report listing the total sales for the month, and the amount of state and county sales tax collected. The state sales tax rate is 4 percent and the county sales tax rate is 2 percent. Design a modular program that asks the user to enter the total sales for the month. Using this amount, the application should calculate and display the following:
-                  • The amount of county sales tax
-                  • The amount of state sales tax
-                  • The total sales tax (county plus state)*/
 #include <iostream>
 #include <string>
 
